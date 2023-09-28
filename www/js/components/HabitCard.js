48af356eb0281d633d17f17f249565f970213f5c
dovh-me <dovh.me@gmail.com>
2023-09-29 04:05:48 +0530

    Implement the habit card component

---
+++ www/js/components/HabitCard.js
@@ -0,0 +1,53 @@
+export const HabitCard = (habit, store, pages) => {
+  const { name, color, goal, goalUnits, progress, status } = habit;
+  const statusImages = {
+    wontDo: "img/status-wont-do.png",
+    noProgress: "img/status-no-progress.png",
+    done: "img/status-done.png",
+  };
+
+  const statusMap = {
+    wontDo: "Won't Do",
+    noProgress: "No Progress",
+    done: "Done",
+  };
+  const el = $(`
+        <article tabindex="0" class="habit-card">
+            <section class="habit-details">
+              <div class="title-section">
+                <div class="habit-color-elem" style="background-color: ${
+                  color ?? "#A2A2A2"
+                }"></div>
+                <h3 class="habit-name">${name ?? "--"}</h3>
+              </div>${statusMap[status] ?? "--"}</div>
+              <div class="habit-summary">
+                <div class="habit-goal summary-item">
+                  <span class="summary-label">Goal: </span>
+                  <span class="goal-value">${goal ?? "--"}</span>
+                  <span class="goal-unit">${goalUnits ?? "--"}</span>
+                </div>
+                <div class="habit-progress summary-item">
+                  <span class="summary-label">Progress: </span>
+                  <span class="goal-value">${progress ?? "--"}</span>
+                  <span class="goal-unit">${goalUnits ?? "--"}</span>
+                </div>
+              </div>
+            </section>
+            <section class="habit-status-icon">
+              <img
+                src="${statusImages[status] ?? statusImages["noProgress"]}"
+                alt="no progress"
+                width="40"
+                height="40"
+              />
+            </section>
+          </article>
+    `);
+  el.on("click", function () {
+    store.clickedHabit = habit;
+    $.mobile.changePage(pages.home, {
+      transition: "fade",
+    });
+  });
+  return el;
+};
