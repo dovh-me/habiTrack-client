42f6bc1a821acd275182a5d708e407bf2d469b86
dovh-me <dovh.me@gmail.com>
2023-10-08 19:24:57 +0530

    Integrating local notifications and relevant permissions

--- www/js/index.mjs
+++ www/js/index.mjs
@@ -1,8 +1,11 @@
 import { RequestHandler } from "./RequestHandler.js";
+import { FullWidthDatePicker } from "./components/FullWidthDatePicker.js";
+import { createHabitPageHandler } from "./pages/create-habit.mjs";
 import { emailVerificationPageHandler } from "./pages/email-verification.mjs";
 import { homePageHandler } from "./pages/home.mjs";
 import { loginPageHandler } from "./pages/login.mjs";
 import { signUpHandler } from "./pages/signup.mjs";
+import { viewHabitPageHandler } from "./pages/view-habit.mjs";
 
 const requestHandler = new RequestHandler();
 const store = {};
@@ -11,52 +14,90 @@ const pages = {
   signUp: "#signUp-page",
   home: "#home-page",
   emailVerification: "#email-verification-page",
+  viewHabit: "#view-habit-page",
 };
 
+// load custom datepickers
+$(".full-width-datepicker-container")
+  .toArray()
+  .forEach((field) => {
+    FullWidthDatePicker(field);
+  });
+
+//----------------
+//----------------
+//----------------
+//----------------
+//----------------
+//----------------
+//----------------
+//----------------
+
+$.validator.addMethod(
+  "valueNotEquals",
+  function (value, element, arg) {
+    console.log("within custom method valueNotEquals", value, arg);
+    return arg !== value;
+  },
+  "You must select a value for this field"
+);
+
+//----------------
+//----------------
+//----------------
+//----------------
+//----------------
+//----------------
+//----------------
+//----------------
+
+// Extra care has been taken to make sure that cordova plugins are not accessed directly within
+// the page handlers
 loginPageHandler(requestHandler, store, pages);
 signUpHandler(requestHandler, store, pages);
 emailVerificationPageHandler(requestHandler, store, pages);
 homePageHandler(requestHandler, store, pages);
+createHabitPageHandler(requestHandler, store, pages);
+viewHabitPageHandler(requestHandler, store, pages);
 
-$(function () {
-  // Show the datepicker when field is clicked
-  $(".date-picker-field").on("click", function (e) {
-    e.currentTarget.showPicker();
-  });
-});
+//----------------
+//----------------
+//----------------
+//----------------
+//----------------
+//----------------
+//----------------
+//----------------
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
-  var permissions = cordova.plugins.permissions;
-  function successCallback() {
-    console.log("Permission granted successfully");
-    alert("Permission granted by the user!!!");
-
-    cordova.plugins.notification.local.requestPermission(function (granted) {
-      cordova.plugins.notification.local.schedule({
-        title: "Meditating",
-        text: "Consistency is the key to develop habits",
-        trigger: { at: new Date() },
-      });
+  function scheduleNotification() {
+    cordova.plugins.notification.local.schedule({
+      title: "Meditating",
+      text: "Consistency is the key to develop habits",
+      trigger: { at: new Date() },
     });
   }
-  function errorCallback() {
-    console.log("There was an error requesting permissions");
-    alert("There was an error requesting permissions!!!");
-  }
-  permissions.requestPermissions(
-    [
-      permissions.POST_NOTIFICATIONS,
-      permissions.ACCESS_NOTIFICATION_POLICY,
-      permissions.RECEIVE_BOOT_COMPLETED,
-      permissions.WAKE_LOCK,
-      permissions.POST_NOTIFICATIONS,
-      permissions.SCHEDULE_EXACT_ALARM,
-    ],
-    successCallback,
-    errorCallback
-  );
+
+  cordova.plugins.notification.local.hasPermission(function (granted) {
+    if (!granted) {
+      alert(
+        `It seems that we are unable to request permission to send notifications.\n\nPlease enable notifications from the settings for app manually. Sorry for the inconvenience caused.\n\nSettings > All Apps > HabitTrack > Notifications`
+      );
+      cordova.plugins.notification.local.requestPermission(function (granted) {
+        console.log(
+          "granted object in request permissions",
+          JSON.stringify(granted)
+        );
+
+        scheduleNotification();
+      });
+      return;
+    }
+    scheduleNotification();
+  });
+
   console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
 }
