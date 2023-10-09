b94ea1a850163b63b6a93f35a909cafc157b1158
dovh-me <dovh.me@gmail.com>
2023-10-10 04:21:54 +0530

    Handle the token within request handler

--- www/js/pages/login.mjs
+++ www/js/pages/login.mjs
@@ -1,10 +1,27 @@
 import { RequestHandler } from "../RequestHandler.js";
 
+const formFields = {
+  "#login-email-input": {
+    requestField: "email",
+  },
+  "#login-password-input": {
+    requestField: "password",
+  },
+};
+
 /**
  *
  * @param {RequestHandler} requestHandler
  */
 export const loginPageHandler = (requestHandler, store, pages) => {
+  const validator = registerLoginFormValidator();
+  Object.keys(formFields).forEach((item) => {
+    $(item).on("change", function (e) {
+      e.preventDefault();
+      validator.element(item);
+    });
+  });
+
   $(pages.login).on("pagebeforeshow", function (e) {
     const isAuthenticated = requestHandler.isAuthenticated;
 
@@ -41,31 +58,57 @@ export const loginPageHandler = (requestHandler, store, pages) => {
   //----------------
   //----------------
   //----------------
+  function registerLoginFormValidator() {
+    const validator = $("#login-form").validate({
+      highlight: function (element, errorClass, validClass) {
+        $(element).removeClass(validClass);
+        $(element.form)
+          .find("label[for=" + element.id + "]")
+          .addClass(errorClass);
+      },
+      unhighlight: function (element, errorClass, validClass) {
+        $(element).addClass(validClass);
+        $(element.form)
+          .find("label[for=" + element.id + "]")
+          .removeClass(errorClass);
+      },
+      rules: {
+        "login-email-input": {
+          email: true,
+          required: true,
+        },
+        "login-password-input": {
+          minlength: 6,
+          maxlength: 20,
+          required: true,
+        },
+      },
+      submitHandler,
+    });
 
-  $("#login-form").on("submit", function (e) {
-    e.preventDefault();
+    function submitHandler(e) {
+      if (!validator.valid()) return;
 
-    const credentials = getLoginFormCredentials();
+      const credentials = getLoginFormCredentials();
 
-    requestHandler
-      .sendLoginRequest(credentials)
-      .then(({ user, accessToken, resultCode }) => {
-        // set the user in the global store
-        store.user = user;
+      requestHandler
+        .sendLoginRequest(credentials)
+        .then(({ user, accessToken, resultCode }) => {
+          // set the user in the global store
+          store.user = user;
 
-        // set the token in the request handler
-        requestHandler.setToken(accessToken);
-
-        const isSuccess = resultCode === "00047";
-        if (isSuccess) {
-          onAuthSuccess();
-        }
-      })
-      .catch((error) => {
-        console.error("There was an error logging in", error);
-        alert("There was an error logging in", error);
-      });
-  });
+          const isSuccess = resultCode === "00047";
+          if (isSuccess) {
+            onAuthSuccess();
+          }
+        })
+        .catch((error) => {
+          console.error("There was an error logging in", error);
+          alert("There was an error logging in", error);
+        });
+    }
+    return validator;
+  }
 
   //----------------
   //----------------
